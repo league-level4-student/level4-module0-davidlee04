9c34946220fc8a0a688eec0f39083c5c8e4aed57
11000783 <league@iMac-3.attlocal.net>
2018-08-22 16:32:45 -0700

    finished (except for game of life and mazemaker, already did in diff repo)

--- src/_01_Crazy_Digital_Painting/CrazyDigitalPainting.java
+++ src/_01_Crazy_Digital_Painting/CrazyDigitalPainting.java
@@ -1,12 +1,17 @@
 package _01_Crazy_Digital_Painting;
 
+import java.awt.Color;
+
 public class CrazyDigitalPainting {
 	//1. Create two final static integers for the width and height of the display.
-
+	static final int width = 700;
+	static final int height = 700;
+	
 	
 	//2. Create a 2D array of Color objects. You will need to import
 	//java.awt.Color. Initialize the size of the array using the 
 	//integers created in step 1.
+	Color[][] colorArr = new Color[width][height];
 	
 	
 	
@@ -17,7 +22,11 @@ public class CrazyDigitalPainting {
 		//   to a new color. The sample image was created using the following 
 		//   pattern:
 		//   colors[i][j] = new Color(i % 256, (i * j) % 256, j % 256);
-		
+		for (int i = 0; i < colorArr.length; i++) {
+			for (int j = 0; j < colorArr.length; j++) {
+				colorArr[i][j] = new Color(i%256, (i*j)%256, j%256);
+			}
+		}
 		//5. Come up with your own pattern to make a cool crazy image.
 		
 		//6. Use the ColorArrayDisplayer class to call the displayColorsAsImage method 
@@ -25,6 +34,7 @@ public class CrazyDigitalPainting {
 	}
 	
 	public static void main(String[] args) {
-		new CrazyDigitalPainting();
+		new ColorArrayDisplayer();
+		ColorArrayDisplayer.displayColorsAsImage(new CrazyDigitalPainting().colorArr);
 	}
 }
